847df79d56bede845d50991a7a53e3c2605ff54a
Adrian Spanu <aspanu@stanford.edu>
2013-01-24 18:25:37 -0800

    Fixed some bugs with the new method of opening files.

--- mrBOLD/Init/sessionGet.m
+++ mrBOLD/Init/sessionGet.m
@@ -92,7 +92,7 @@ switch param
         % This can now be found in the mrSESSION.inplanes.inplanePath
         % location
         
-        val = s.inplanes.path;
+        val = s.inplanes.inplanePath;
         
         if isempty(val), warning('Inplane path not found'); end %#ok<WNTAG>
         
