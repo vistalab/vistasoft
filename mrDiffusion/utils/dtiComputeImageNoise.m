db4b32a2c15e85224f5219fbb081921ea4981a55
Bob Dougherty <bobd@stanford.edu>
2013-02-20 14:31:19 -0800

    Clarified the comments.

--- mrDiffusion/utils/dtiComputeImageNoise.m
+++ mrDiffusion/utils/dtiComputeImageNoise.m
@@ -19,20 +19,24 @@ if ~exist('noiseCalcMethod','var') || isempty(noiseCalcMethod)
 end
 
 if strcmp('corner', noiseCalcMethod)
-    
+
     % According to Henkelman (1985), the expected signal variance (sigma) can
     % be computed as 1.5267 * SD of the background (thermal) noise.
+    % See also Gudbjartsson & Patz (1995). The Rician Distribution of Noisy MRI Data
+    % (http://www.ncbi.nlm.nih.gov/pmc/articles/PMC2254141/), where this term
+    % is defined as 1/sqrt(2-pi/2) = 1.5264. Henkelman seems to have rounded off in
+    % his calculations to get 1.5267. We'll compute the more accurate value here.
     sz = size(dwRaw.data);
     x  = 10;
     y  = 10;
     z  = round(sz(3)/2);
-    
+
     [x,y,z,s] = ndgrid(x-5:x+5, y-5:y:5, z-5:z+5, 1:sz(4));
     noiseInds = sub2ind(sz, x(:), y(:), z(:), s(:));
     sigma     = 1.5267 * std(double(dwRaw.data(noiseInds)));
-    
+
 elseif strcmp('b0', noiseCalcMethod)
-    
+
     % Number of volumes in the dw dataset
     numVols   = size(dwRaw.data,4);
     % Get brainmask indices
@@ -40,13 +44,13 @@ elseif strcmp('b0', noiseCalcMethod)
     % preallocate a 2d array (with a 2nd dimension that is a singleton). The
     % first dimension is the number of volumes and the 3rd is each voxel
     % (within the brain mask).
-    data      = zeros(numVols,1,length(brainInds));  
+    data      = zeros(numVols,1,length(brainInds));
     % Loop over the volumes and assign the voxels within the brain mask to data
     for ii=1:numVols
         tmp = double(dwRaw.data(:,:,:,ii));
         data(ii,1,:) = tmp(brainInds);
     end
-    
+
     % Find which volumes ar b=0
     b0inds = find(bvals ==  0);
     n = length(b0inds);
@@ -56,12 +60,12 @@ elseif strcmp('b0', noiseCalcMethod)
     % this needs to be rescaled. Henkelman et al. (1985) suggest that this
     % aproaches the true noise as the signal increases.
     sigma = median(std(dataB0,0,1));
-    
+
     % std of a sample underestimates sigma (see http://nbviewer.ipython.org/4287207/)
     % This can be very big for small n (e.g., 20% for n=2)
     % We can compute the underestimation bias:
     bias = sigma * (1 - sqrt(2 / (n-1)) * (gamma(n / 2) / gamma((n-1) / 2)));
-    
+
     % and correct for it:
     sigma = sigma + bias;
 end
