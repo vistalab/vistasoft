c2d61b695afcb279a40d8e47491c672308c811dd
Adrian Spanu <aspanu@stanford.edu>
2013-03-14 16:41:50 -0700

    Fixed a bug in niftiGet related to dimensions of >3 elements.

--- fileFilters/nifti/niftiGet.m
+++ fileFilters/nifti/niftiGet.m
@@ -12,11 +12,32 @@ param = mrvParamFormat(param);
 val = [];
 
 switch param
+    case 'calmin'
+        if isfield(ni, 'cal_min')
+            val = ni.cal_min; 
+        else
+            warning('vista:niftiError', 'No minimum value information found in nifti. Returning empty');
+            val = [];
+        end
+    case 'calmax'
+        if isfield(ni, 'cal_max')
+            val = ni.cal_max; 
+        else
+            warning('vista:niftiError', 'No maximum value information found in nifti. Returning empty');
+            val = [];
+        end
     case 'data'
         if isfield(ni, 'data')
             val = ni.data; 
         else
             warning('vista:niftiError', 'No data information found in nifti. Returning empty');
+            val = [];
+        end
+    case 'datatype'
+        if isfield(ni, 'data_type')
+            val = ni.data_type; 
+        else
+            warning('vista:niftiError', 'No data_type information found in nifti. Returning empty');
             val = [];
         end
     case 'dim'
@@ -26,6 +47,13 @@ switch param
             warning('vista:niftiError', 'No dimension information found in nifti. Returning empty');
             val = [];
         end
+    case 'fname'
+        if isfield(ni, 'fname')
+            val = ni.fname; 
+        else
+            warning('vista:niftiError', 'No file name information found in nifti. Returning empty');
+            val = [];
+        end 
     case 'freqdim'
         if isfield(ni, 'freq_dim')
             val = ni.freq_dim; 
@@ -33,6 +61,13 @@ switch param
             warning('vista:niftiError', 'No frequency dimension information found in nifti. Returning empty');
             val = [];
         end 
+    case 'ndim'
+        if isfield(ni, 'ndim')
+            val = ni.ndim; 
+        else
+            warning('vista:niftiError', 'No number of dimensions information found in nifti. Returning empty');
+            val = [];
+        end
     case 'numslices'
         if isfield(ni, 'slice_end') && isfield(ni,'slice_start')
             val = ni.slice_end - ni.slice_start + 1; 
@@ -79,8 +114,11 @@ switch param
     case 'slicedims'
         %Get the slice dimensions (i.e. the dimensions of each 2-D matrix
         %making up a slice)
-        totDim = niftiGet(ni,'dim');
-        val = totDim(setdiff(1:length(totDim),niftiGet(ni,'slice dim')));
+        val = niftiGet(ni,'dim');
+        sliceDimLogical = true(size(val));
+        sliceDimLogical(niftiGet(ni,'phase dim')) = 0;
+        sliceDimLogical(niftiGet(ni,'freq dim')) = 0;
+        val(sliceDimLogical) = [];
         %val(1) = totDim(niftiGet(ni,'freq Dim'));
         %val(2) = totDim(niftiGet(ni,'phase Dim'));
  	case 'sto_ijk'
