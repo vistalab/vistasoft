cbfd7ffc0d092754fe68ae3caa0102c6f28481c7
frakkopesto <frakkopesto@gmail.com>
2013-04-04 16:06:55 -0700

    ENH: Fixed a bug for path of output files in mrtrix_init.m fixed the function mrtrix_track_roi2roi.m

--- external/mrtrix/mrtrix_track_roi2roi.m
+++ external/mrtrix/mrtrix_track_roi2roi.m
@@ -1,4 +1,4 @@
-function [status, results] = mrtrix_track_roi2roi(files, roi1, roi2, seed, mask, mode, n, n_max, bkgrnd, verbose)
+function [pdb_file, status, results] = mrtrix_track_roi2roi(files, roi1, roi2, seed, mask, mode, n, n_max, bkgrnd, verbose)
 
 % Provided a csd estimate, generate estimates of the fibers starting in the
 % seed region, touching both roi1 and roi2.
@@ -28,26 +28,25 @@ if notDefined('clobber'), clobber = false;end
 
 % Choose the tracking mode (probabilistic or stream)
 switch mode
-  case {'prob','probabilistic tractography')
+  case {'prob','probabilistic','probabilistic tractography','p'}
     mode_str = 'SD_PROB';
-  case {'stream','deterministic tractogrpahy based on spherical deconvolution'}
+  case {'stream','deterministic','deterministic tractogrpahy based on spherical deconvolution','d'}
     mode_str = 'SD_STREAM';
-  case {'tensor','deterministic tractogrpahy based on a tensor model'}
+  case {'tensor','deterministic tractogrpahy based on a tensor model','t'}
     mode_str = 'DT_STREAM';
   otherwise
     error('Input "%s" is not a valid tracking mode', mode); 
 end
 
 % Track, using deterministic estimate: 
-tck_file = strcat(strip_ext(csd), '_' , strip_ext(roi1), '_', strip_ext(roi2), '_', ...
+tck_file = strcat(strip_ext(files.csd), '_' , strip_ext(roi1), '_', strip_ext(roi2), '_', ...
                             strip_ext(seed) , '_', strip_ext(mask), '_', mode, '.tck'); 
                           
 % Generate a UNIX command string.                          
 switch mode_str
   case {'SD_PROB', 'SD_STREAM'}
     cmd_str = sprintf('streamtrack -seed %s -mask %s -include %s -include %s %s %s %s -number %d -maxnum %d -stop',...
-      seed, mask, roi1, roi2, mode_str, files.csd, tck_file, n, n_max);
-    
+      seed,mask, roi1, roi2, mode_str, files.csd, tck_file, n, n_max);
     % The following lines of code could be intergated here, to allow for
     % tracking between ROIs using a tensor-based deterministic tractography
     %   case {'DT_STREAM'}
@@ -71,6 +70,8 @@ if ~(exist(tck_file,'file') ==2)  || clobber == 1
     [status, results] = mrtrix_cmd(cmd_str, bkgrnd, verbose);
 else
   fprintf('\nFound fiber tract file: %s.\n Loading it rather than retracking',tck_file)
+  status = 0;
+  results = 'loaded file from disk';
 end
 
 % Convert the tracks to pdb format:
