f2839344414e8b318c59effe575152623ef1730f
Jonathan Winawer <rewaniw@gmail.com>
2013-08-01 17:07:40 -0700

    rearranged the argument checks at the top of the function to avoid dependencies on variables that are not yet defined. also updated some syntax

--- mrBOLD/EventRelated/er_groupScans.m
+++ mrBOLD/EventRelated/er_groupScans.m
@@ -1,18 +1,18 @@
-function view = er_groupScans(view,scans,assignFlag,dt);
-% view = er_groupScans(view,[scans],[assignFlag],[datatype]);
+function vw = er_groupScans(vw,scans,assignFlag,dt)
+% vw = er_groupScans(vw,[scans],[assignFlag],[datatype])
 %
 % Create a functional grouping of scans for future
-% analyses / plotting. 
+% analyses / plotting.
 %
 % This is useful if you have run more than one different
 % scan type in a session, and want to plot the concatenated
 % tSeries from similar scans together, or apply analyses (like
 % GLM) to them, without manually selecting them each time.
 %
-% Note that the assignment can cross data type: you can group 
-% a bunch of Original scans together, then assign that group 
+% Note that the assignment can cross data type: you can group
+% a bunch of Original scans together, then assign that group
 % to the average of those scans in Averages, for instance. Then
-% you can e.g. look at a corAnal from the average scan, invoke 
+% you can e.g. look at a corAnal from the average scan, invoke
 % time course UI to look at the time series from the original scans,
 % and not have to switch back to the Originals data type.
 %
@@ -31,53 +31,46 @@ global dataTYPES HOMEDIR;
 
 names = {dataTYPES.name};
 
-cdt = view.curDataType;
+cdt = vw.curDataType;
+
+
+if ieNotDefined('assignFlag'),assignFlag = 2; end
+
+% if dt hasn't already been assigned, set to current
+if ieNotDefined('dt'), dt = cdt; end
+
+% if a string specified for dt, find # of data type
+if ischar(dt), dt = existDataType(dt); end
 
 if ieNotDefined('scans')
     % get scans w/ dialog:
-	
-	if assignFlag==2
-		% first choose data type
-		dt = menu('Group scans in which data type?',names);
-	else
-		dt = view.curDataType;
-	end
-	
+    
+    % first choose data type
+    if assignFlag==2, dt = menu('Group scans in which data type?',names);
+    else              dt = vw.curDataType; end
+    
     % get list of scans in this data type
-    view.curDataType = dt;
-    scans = er_selectScans(view);
-    view.curDataType = cdt;
+    vw.curDataType = dt;
+    scans = er_selectScans(vw);
+    vw.curDataType = cdt;
 end
 
-if ieNotDefined('assignFlag')
-    assignFlag = 2;
-end
-
-% if dt hasn't already been assigned, set to current
-if ieNotDefined('dt')
-    dt = cdt;
-end
-
-% if a string specified for dt, find # of data type
-if ischar(dt)
-    dt = existDataType(dt);
-end
 
 switch assignFlag
-    case 1, tgtScans = viewGet(view, 'curScan');
-            tgtDt = cdt;
+    case 1, tgtScans = viewGet(vw, 'curScan');
+        tgtDt = cdt;
     case 2, tgtScans = scans;
-            tgtDt = dt;
+        tgtDt = dt;
     otherwise,
-        error('assignFlag must be 1 or 2.');            
+        error('assignFlag must be 1 or 2.');
 end
-   
+
 % check if there's already a scanGroup field
 % in dataTYPES.scanParams for the tgt data type:
 for j = 1:length(dataTYPES)
     if ~isfield(dataTYPES(j).scanParams(end),'scanGroup')
         dataTYPES(j).scanParams(end).scanGroup = [];
-	end
+    end
 end
 
 % assign the scans to the scanGroup field for the relevant
