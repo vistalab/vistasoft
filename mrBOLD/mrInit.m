1d599bc0a2edde06f0b204e0bb4f3fb4db7ad658
arokem <arokem@gmail.com>
2013-08-08 11:05:19 -0700

    Whitespace.

--- mrBOLD/mrInit.m
+++ mrBOLD/mrInit.m
@@ -247,12 +247,11 @@ end
 % slice timing correction
 if params.sliceTimingCorrection==1
     if isfield(params, sliceOrder)
-        slices = params.sliceOrder; 
+        slices = params.sliceOrder;
     else
         slices = []; % Default behavior assumes 1:nSlices
     end
     INPLANE{1} = AdjustSliceTiming(INPLANE{1}, 0, [], slices);
-    
     INPLANE{1} = selectDataType(INPLANE{1}, 'Timed');
 end
 
