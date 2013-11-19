4c23b8e97f48fc03380c0afc7ca1039efdbd481a
arokem <arokem@gmail.com>
2013-11-19 11:21:07 -0800

    Make output file an optional input.

--- fileFilters/vtk/vtk_fromClass.m
+++ fileFilters/vtk/vtk_fromClass.m
@@ -1,29 +1,39 @@
-function vtk_fname = vtk_fromClass(fname)
+function vtk_fname = vtk_fromClass(in_fname, out_fname)
 
+% function vtk_fname = vtk_fromClass(in_fname, [out_fname])
 %
 % Convert a mrGray .class file into VTK format
 % 
 %  Parameters
 %  ----------
-%  fname: str
+%  in_fname : str
 %     Full path to a .class file 
-%  
+%  out_fname : str (optional)
+%     Full path to a vtk output file.
+%
 %  Returns
 %  -------
-% vtk_fname: str
+%  vtk_fname : str
 %     Full path to the output file
 % 
-% Notes
-% ------
-% The specification of the VTK file format is take from here: 
-% http://wideman-one.com/gw/brain/fs/surfacefileformats.htm
+%  Notes
+%  ------
+%  The specification of the VTK file format is take from here: 
+%  http://wideman-one.com/gw/brain/fs/surfacefileformats.htm
+% 
+%  Example
+%  -------
+%  fName = '/biac4/wandell/biac2/wandell2/data/dti/dti_y1_old/at040918/left/20070209/leftWhole.class'
+%  vtk_fromClass(fName);
 
-%fName = '/biac4/wandell/biac2/wandell2/data/dti/dti_y1_old/at040918/left/20070209/leftWhole.class'
-
-% Put the output file in the same location with the right extension: 
-vtk_fname = [fname(1:end-5), 'vtk']; 
-
-msh = meshBuildFromClass(fname);
+if(~exist('out_fname','var')|isempty(in_fname))
+    % Put the output file in the same location with the right extension: 
+    vtk_fname = [in_fname(1:end-5), 'vtk']; 
+else
+    vtk_fname = out_fname
+end
+    
+msh = meshBuildFromClass(in_fname);
 vv = msh.vertices; 
 tt = msh.triangles; 
 
