d9b01671a7905d01d7c10bd8b3da237021fd402c
Jonathan Winawer <jonathan.winawer@nyu.edu>
2014-06-22 16:51:32 -0400

    added a flipdim for dimension 2 in the migration tool. This was needed because at some point our preferred orienation for display purposes switched from ARS to PRS. This made no difference for data sets initialized with new code, but it did matter for data sets migrated from old vista formats

--- fileFilters/nifti/niftiCreateXform.m
+++ fileFilters/nifti/niftiCreateXform.m
@@ -1,5 +1,33 @@
 function [xform] = niftiCreateXform(nii,xformType)
-%First create a specified transform onto the supplied nifti struct.
+%Create a specified transform onto the supplied nifti struct. For
+% data in slice format, we specify our output data xform according to
+% certain rules. Our preferred orientation is PRS, but we do not want to
+% change the slice dimension, so we apply these rules:
+%
+% 1. The slice dimension is third
+% 2. The slice dimension does not change direction or axis
+% 3. Except for the slice dimension, we remap any of LIA => RSP
+% 4. If slice dimension is not L or R, then 2nd dimension is R
+% 5. If slice dimension is not A or P, then 1st dimension is P
+%
+% For example,
+% 
+% xformIn = [...
+%  'ASR' ; 'AIR' ; 'PSR' ; 'PIR' ; 'SAR' ; 'SPR' ; 'IAR' ; 'IPR' ;
+%  'ASL' ; 'AIL' ; 'PSL' ; 'PIL' ; 'SAL' ; 'SPL' ; 'IAL' ; 'IPL' ;
+%  'ARS' ; 'ALS' ; 'PRS' ; 'PLS' ; 'RAS' ; 'RPS' ; 'LAS' ; 'LPS' ;
+%  'ARI' ; 'ALI' ; 'PRI' ; 'PLI' ; 'RAI' ; 'RPI' ; 'LAI' ; 'LPI' ;
+%  'SRA' ; 'SLA' ; 'IRA' ; 'ILA' ; 'RSA' ; 'RIA' ; 'LSA' ; 'LIA' ;
+%  'SRP' ; 'SLP' ; 'IRP' ; 'ILP' ; 'RSP' ; 'RIP' ; 'LSP' ; 'LIP' ;
+%  ];
+% 
+% xformOut = xformIn;
+% for ii = 1:length(xformIn)
+%  xformOut(ii,:) = niftiCreateStringInplane(xformIn(ii,:),3);
+%  fprintf('%s=>%s\n',xformIn(ii,:), xformOut(ii,:));
+% end
+
+% See niftiCreateStringInplane for the computation.
 %
 % USAGE
 %  nii = readNifti(niftiFullPath);
@@ -13,6 +41,7 @@ function [xform] = niftiCreateXform(nii,xformType)
 % RETURNS
 %  Xform matrix in the form a quaternion
 %
+% See also niftiCreateStringInplane
 %
 % Copyright Stanford VistaLab 2013
 
