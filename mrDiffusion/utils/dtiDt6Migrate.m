2bb5b88fb39a958ea3bfdff224f4f15931b4f3e1
Brian Wandell <wandell@stanford.edu>
2014-09-07 15:42:32 -0700

    Edited comments

--- mrDiffusion/utils/dtiDt6Migrate.m
+++ mrDiffusion/utils/dtiDt6Migrate.m
@@ -1,21 +1,24 @@
 function dtiDt6Migrate(rawDir)
+% Fix the file dependencies stored in a dt6 structure 
 %
-% Migrate a dt6 structure from a previosu cfolder to the current folder. 
-%
-%      >> dtiDt6Migrate
+% The dt6.files.aligned* are absolute paths, not relative.  This causes
+% problems when we move to a new system.  We should be able to move the
+% folder and have the code run.
 %
 % This function will update all the paths in the dt6 structure after a dt6
 % file is moved to a new folder.
 %
-% USAGE:
-%    (1) move a dt6 directory structure from its original location to a new
-%        location.
-%    (2) cd into the new location of the dt6.mat file
-%    (3) type: dtiDt6Migrate, to update all the path information inside the
-%        dt6.mat strucutre.
-%    (4) A new file called dt6.mat is created in the current directory.
-%        A copy of the dt6 file before any change was made is copied over
-%        to a file dt6_old.mat
+% Example:
+%    Change to the dt6 file's folder and type
+%    
+%          dtiDt6Migrate
+%
+%   This creates a new file called dt6.mat is created in the current
+%   directory. A copy of the dt6 file before any change was made is copied
+%   over to a file dt6_old.mat
+%
+% See also:
+%    dtiLoadDt6
 %
 % Written by Franco Pestilli (c) Vistasoft, Stanford University
 
