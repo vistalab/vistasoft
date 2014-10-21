903c4aea92fde4910ec5c9dfee4d5f2e2a779690
Michael Perry <lmperry@stanford.edu>
2014-10-21 14:26:29 -0700

    Initialize FileList as empty cell array.

--- utilities/file/readFileList.m
+++ utilities/file/readFileList.m
@@ -3,6 +3,7 @@ function FileList= readFileList(filename)
 %Reads list of files line by line into a cell array
 fid=fopen(filename, 'r');
 count=0;
+FileList = {};
 while 1
     count=count+1;
     tline=fgetl(fid);
