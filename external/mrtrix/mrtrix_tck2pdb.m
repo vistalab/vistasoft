87c9dd0820779ea6d21866b30e1209035a3fa14a
htakemur <htakemur@stanford.edu>
2015-05-26 10:14:38 +0900

    Modified the issue in code tck2pdb

--- external/mrtrix/mrtrix_tck2pdb.m
+++ external/mrtrix/mrtrix_tck2pdb.m
@@ -1,4 +1,4 @@
-function fg = mrtrix_tck2pdb(tck_file, pdb_file, fiberPointStride)
+function fg = mrtrix_tck2pdb(tck_file, pdb_file)
 %
 %    fg = mrtrix_tck2pdb(tck_file, pdb_file)
 %
@@ -8,23 +8,15 @@ function fg = mrtrix_tck2pdb(tck_file, pdb_file, fiberPointStride)
 % INPUTS
 %   tck_file - string, full path to the tck file. 
 %   pdb_file - string, full path to the resulting pdb file. 
-%   fiberPointStride - fiberPointStride: if <=1, then all fiber points will be returned. If 2,
-%   every 2nd point will be returned, etc. The default will try to get you
-%   close to a 1mm step size.
-% 
+
 % Notes
 %  see dtiImportFibersMrtrix.m
 %
 % Franco Pestilli, Ariel Rokem and Bob Dougherty Stanford University 
 
-if notDefined('fiberPointStride')
-   fiberPointStride = eye(4);
-else
-end
-
 if exist(tck_file,'file')
-   fg = dtiImportFibersMrtrix(tck_file, fiberPointStride);
-   mtrExportFibers(fg, pdb_file);
+   fg = dtiImportFibersMrtrix(tck_file);
+   mtrExportFibers(fg, pdb_file,eye(4));
 else
     error('[%s] Aborting. Cannot find .tck file : %s .',mfilename,tck_file);
 end
