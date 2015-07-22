7fea574667d77e76100d06a0c5da56785308f288
Michael Perry <lmperry@stanford.edu>
2015-07-22 10:04:03 -0700

    Add default values to comments

--- external/camino/camTrackPico.m
+++ external/camino/camTrackPico.m
@@ -1,22 +1,27 @@
 function camTrackPico(seed_filename, numiteration, pdfs_filename, picotract_filename, curv_thresh, stepsize, curveinterval)
 %
-% camTrackPico(seed_filename, numiteration, pdfs_filename, picotract_filename, curv_thresh, stepsize, curveinterval)
+% camTrackPico(seed_filename, numiteration, pdfs_filename, picotract_filename, [curv_thresh=90], [stepsize=1], [curveinterval=5])
 %
 % Execute the PICo tracking using Camino
 %
 % INPUTS: 
 %   seed_filename: The nifti file used as the seed for PICo tracking.This
-%                nifti file should the identical resolution and data matrix
-%                size with original dwi data.
+%                  nifti file should have the identical resolution and data 
+%                  matrix size as the original dwi data.
 % 
-%   numiteration: The number of iteration in PICo pdfs_filename: The filename
-%                 of probability density functions picotract_filename: The
-%                 file name for streamline file in Bfloat format
-%                 curv_thresh: curvature threshold
+%   numiteration:       The number of iterations in PICo 
 % 
-%   stepsize: tracking step size curveinterval: The curvature threshold will
-%             not be applied if streamline length is shorter than
-%             curveinterval
+%   pdfs_filename:      The filename of probability density functions 
+% 
+%   picotract_filename: The filename for streamline file in Bfloat format
+%                 
+%   curv_thresh:        curvature threshold (default = 90)
+% 
+%   stepsize:           tracking step size (default = 1)
+% 
+%   curveinterval: The curvature threshold will not be applied if 
+%                  streamline length is shorter than the curveinterval
+%                  (default = 5)
 % 
 % For details of PICo Tracking parameter settings, please check Camino's
 % website http://cmic.cs.ucl.ac.uk/camino//index.php?n=Man.Track
