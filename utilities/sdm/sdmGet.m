d6def7d8697d9748c3176cb152aa2084e6e594ac
Michael Perry <lmperry@stanford.edu>
2015-09-25 13:52:02 -0700

    Utilities for getting/putting data to/from SDM.

---
+++ utilities/sdm/sdmGet.m
@@ -0,0 +1,29 @@
+function fName = sdmGet(pLink,uName,fName)
+% Retrieve a file from an sdm instance
+%
+%   sdmFile = sdmGet(pLink,uName,[sdmFile])
+%
+% Inputs
+%  pLink:  Permalink from the SDM
+%  uName:  The sdm user name with appropriate permission
+%  fName:  Location and/or filename to write the file
+%
+% Return
+%  fName:  Returned file
+%
+% Example:
+%
+% LMP/BW Vistasoft Team, 2015
+
+link = [pLink,uName];
+
+%% Use urlwrwite to retrieve the data and store it
+if notDefined('sdmFile') 
+    [~, f, e] = fileparts(pLink);
+    t_e = explode('?',e);
+    fName = fullfile(tempdir,[ f, t_e{1}]);
+end
+
+urlwrite(link,fName);
+
+end
