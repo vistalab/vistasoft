70a0061293542f460aa9b5ccfa8854a28fa5c333
Michael Perry <lmperry@stanford.edu>
2016-01-22 16:56:10 -0800

    Adopt new auth token scheme for sdmGet and sdmPut.
    With the addition of sdmAuth users will now have to authenticate via
    google prior to being able to use sdmGet and sdmPut. In addition they
    will be required to configure a given instance (e.g.,
    sni-sdm.stanford.edu) by entering the client_secret/id for that instance -
    which they will be prompted for the first time they run sdmGet or put.

--- utilities/sdm/sdmPut.m
+++ utilities/sdm/sdmPut.m
@@ -1,12 +1,11 @@
-function [status, result] = sdmPut(pLink,uName,fName)
+function [status, result] = sdmPut(pLink,fName)
 % Attach a file to the permalink location
 %
 %      sdmPut(pLink,uName,fname)
 %
 % Inputs: 
-%  pLink:   Permalink from an SDM instance, session or acquisition file
+%  pLink:   Permalink from an SDM instance for session or acquisition file
 %  fname:   Name of the file on disk to attach/put/upload 
-%  uName:   Login ID of the user with permission to upload to the pLink
 % 
 % Outputs:
 %  status:  Boolean indicating success (0) or failure (~=0)
@@ -14,21 +13,19 @@ function [status, result] = sdmPut(pLink,uName,fName)
 %
 % 
 % Example:
-%   pLink ='https://vista.scitran.stanford.edu/api/acquisitions/559e9c86c81ba9de1e95ad61/file/1.2.840.113619.2.283.4120.7575399.26065.1300464087.922_1_dicom.tgz?user='
-%   uName = 'someuser@stanford.edu';
+%   pLink ='https://sni-sdm.stanford.edu/api/acquisitions/559e9c86c81ba9de1e95ad61/file/1.2.840.113619.2.283.4120.7575399.26065.1300464087.922_1_dicom.tgz'
 %   fName = '/Path/to/some/file/on/disk'
 %   
-%   [status, result] = sdmPut(pLink,uName,fName);
+%   [status, result] = sdmPut(pLink,fName);
 %
-% LMP/BW Vistasoft Team, 2015
+% LMP/BW Vistasoft Team, 2015-16
 
 
 %% Parse inputs
 
-% Handle new permalinks which don't have '?user='
-if isempty(strfind(pLink, '?user='))
-    pLink = [pLink, '?user='];
-end
+% Handle permalinks which may have '?user='
+pLink = explode('?', pLink);
+pLink = pLink{1};
 
 % Build the url from the permalink by removing the endpart
 url = fileparts(pLink);
@@ -81,14 +78,19 @@ else
 end
 
 
-%% Build and execute the command
+%% Get upload token
+fprintf('Authenticating...')
+[token, ~] = sdmAuth();
+fprintf('done.\n');
 
-% Build - Example: curl -X PUT --data-binary @<file_name_on_disk> -H "Content-MD5:<md5_check_sum>" -H "Content-Type:application/octet-stream" "<URL_and_file_name>?user=<user_name>&flavor=attachment"
-curl_cmd = sprintf('/usr/bin/curl -v -X PUT --data-binary @%s -H "Content-MD5:%s" -H "Content-Type:application/octet-stream" "%s?user=%s&flavor=attachment"\n',fName,checkSum,urlAndName,uName);
+
+%% Build and execute the curl command
+
+curl_cmd = sprintf('/usr/bin/curl -v -X PUT --data-binary @%s -H "Content-MD5:%s" -H "Content-Type:application/octet-stream" -H "Authorization:%s" "%s?flavor=attachment"\n', fName, checkSum, token, urlAndName);
 
 % Execute the command
 fprintf('Sending... ');
-[status,result] = system(curl_cmd);
+[status, result] = system(curl_cmd);
 
 % Let the user know if it worked
 if status
