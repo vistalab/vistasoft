5297b2c88849119c465fdbd44b4cb0ac02b819d0
Jonathan Winawer <jonathan.winawer@nyu.edu>
2016-07-14 08:27:13 -0400

    additional input options added, allowing user to specify destination directory and whether or not to forceOverwrite folder if one already exists

--- mrTest/mrtInstallSampleData.m
+++ mrTest/mrtInstallSampleData.m
@@ -1,6 +1,7 @@
-function dataDir = mrtInstallSampleData(sourceFolder, projectName)
+function dataDir = mrtInstallSampleData(sourceFolder, projectName, dFolder, forceOverwrite)
 %MRTINSTALLSAMPLEDATA Install sample data set on local path.
-%   dataDir = MRTINSTALLSAMPLEDATA(sourceFolder, projectName) 
+%   dataDir = MRTINSTALLSAMPLEDATA(sourceFolder, projectName, ...
+%      [dFolder], [forceOverwrite]) 
 %
 %   The project will be installed in the vistasoft local directory:
 %   fullfile(vistaRootPath, 'local')
@@ -10,11 +11,16 @@ function dataDir = mrtInstallSampleData(sourceFolder, projectName)
 %       repository/vistasoft/vistadata/<sourceFolder>/<projectName>
 %
 %    Inputs
-%     sourceFolder: name of remote folder in which the project is stored
-%                   Examples: 'functional' | 'anatomy' | 'diffusion'
-%     projectName:  name of project to download
-%                   Examples: 'mrBOLD_01' | 'prfInplane'
-%
+%     sourceFolder:     Name of remote folder where project is stored
+%                         Examples: 'functional' | 'anatomy' | 'diffusion'
+%     projectName:      Name of project to download
+%                         Examples: 'mrBOLD_01' | 'prfInplane'
+%     dFolder:          Destination folder 
+%                         [default = fullfile(vistaRootPath, 'local')]
+%     forceOverwrite:   If true, unzip the project even if project folder
+%                           is found. If false, do not unzip if project
+%                           folder is found. 
+%                          [default = true]
 %    Outputs
 %      datadir:  full path to installed project folder
 %
@@ -26,6 +32,10 @@ function dataDir = mrtInstallSampleData(sourceFolder, projectName)
 %
 %    See also: MRVTEST
 
+% Check inputs
+if notDefined('forceOverwrite'), forceOverwrite = true; end
+if notDefined('dFolder'), dFolder = fullfile(vistaRootPath, 'local'); end
+
 % This creates the object, using the configuration data in the file
 % rdt/rdt-config-vistasoft.json
 rd = RdtClient('vistasoft');
@@ -34,15 +44,20 @@ rd = RdtClient('vistasoft');
 rd.crp(sprintf('/vistadata/%s/', sourceFolder));
 
 % Download the zip file
-dFolder = fullfile(vistaRootPath, 'local');
 rd.readArtifact(projectName, 'type','zip', 'destinationFolder',dFolder);
-
-% Unzip
-zipfile = fullfile(dFolder, sprintf('%s.zip', projectName));
-unzip(zipfile, dFolder);
 
 % Return the directory containing the unzipped data
 dataDir = fullfile(dFolder, projectName);
+
+% Unzip 
+if exist(dataDir, 'dir') && ~forceOverwrite
+    % If the project directory already exists and user has requested NOT to
+    % force overwrite, then we do not unzip
+    fprintf('Data directory %s already exists. Skipping unzip.\n', dataDir);
+else
+    zipfile = fullfile(dFolder, sprintf('%s.zip', projectName));
+    unzip(zipfile, dFolder);
+end
 
 if ~exist(dataDir, 'dir')
     warning('Cannot locate unzipped data directory %s', dataDir)
