60c9231ef00bb3ca859ee3b427275f5fba2a4330
Jonathan Winawer <jonathan.winawer@nyu.edu>
2016-09-14 10:06:16 -0400

    update freesurfer mesh tutorial to do visualization in matlab in addition to mrMesh

--- tutorials/mesh/t_meshFromFreesurfer.m
+++ tutorials/mesh/t_meshFromFreesurfer.m
@@ -70,7 +70,7 @@ save(fullfile(savepth, 'fsLeftWhiteMeshUsmoothed'), 'msh');
 
 % rename the mesh for later visualization
 leftMsh = msh;
-
+leftMsh.title = 'Left Mesh, Gray/White Boundary';
 
 % Now do the same for the right mesh
 fsSurface = fullfile(fsPath, 'ernie', 'surf', 'rh.white');
@@ -82,35 +82,71 @@ save(fullfile(savepth, 'fsRightWhiteMeshUsmoothed'), 'msh');
 
 % rename the mesh for later visualization
 rightMsh = msh;
+rightMsh.title = 'Right Mesh, Gray/White Boundary';
 
 % Let's do a pial surface just for comparison (but we won't save it)
 fsSurface = fullfile(fsPath, 'ernie', 'surf', 'rh.pial');
 rightPialMsh = fs_meshFromSurface(fsSurface);
+rightPialMsh.title = 'Right Mesh, Gray/Pial Boundary';
 
 
 %% Visualize 
-if prefsVerboseCheck
     
-    % View the meshes
-    meshVisualize(leftMsh)
-    
-    % This call should open a new application, mrMeshMac or a related name
-    % (depending on your OS), if it is not already open. Then it will open a
-    % new window and show the mesh. You can rotate, zoom, and translate with
-    % the appropriate mouse actions. We BELIEVE that the mouse buttons are as
-    % follows:
-    %   Rotation:  left mouse button plus movement
-    %   Zoom:      right mouse button plus up/down movement
-    %   Translate: left and right together plus movement
-    
-    % Visualize the right mesh
-    meshVisualize(rightMsh)    
-    
-    % Visualize the right pial mesh. Note that there is less space in the
-    % sulci, because the mesh is at the gray-pial boundary rather than the
-    % white-gray boundary
-    meshVisualize(rightPialMsh)
+% View the meshes
+meshVisualize(leftMsh)
 
+% This call should open a new application, mrMeshMac or a related name
+% (depending on your OS), if it is not already open. Then it will open a
+% new window and show the mesh. You can rotate, zoom, and translate with
+% the appropriate mouse actions. We BELIEVE that the mouse buttons are as
+% follows:
+%   Rotation:  left mouse button plus movement
+%   Zoom:      right mouse button plus up/down movement
+%   Translate: left and right together plus movement
+
+% Visualize the right mesh
+meshVisualize(rightMsh)
+
+% Visualize the right pial mesh. Note that there is less space in the
+% sulci, because the mesh is at the gray-pial boundary rather than the
+% white-gray boundary
+meshVisualize(rightPialMsh)
+
+%% Alernative visualiztion in Matlab
+
+for msh = {leftMsh, rightMsh, rightPialMsh}
+    
+    figure,
+    
+    % Faces (also called triangles) are defined by 3 points, each of
+    % which is an index into the x, y, z vertices
+    faces = msh{1}.triangles' + 1; % we need to 1-index rather than 0-index for Matlab
+    
+    % The vertices are the locations in mm spacing
+    x     = msh{1}.vertices(1,:)';
+    y     = msh{1}.vertices(2,:)';
+    z     = msh{1}.vertices(3,:)';
+    
+    % The colormap will, by default, paint sulci dark and gyri light
+    c     = msh{1}.colors(1,:)';
+    
+    % Render the triangle mesh
+    tH = trimesh(faces, x,y,z);
+    
+    % Make it look nice
+    set(tH, 'LineStyle', 'none', 'FaceColor', 'interp', 'FaceVertexCData',c)
+    axis equal off; colormap gray; set(gca, 'CLim', [0 255])
+    
+    % Lighting to make it look glossy
+    light('Position',100*[0 1 1],'Style','local')
+    lighting gouraud
+    
+    % Which mesh are we plotting?
+    title(msh{1}.title)
+    
+    % Rotate it
+    set(gca, 'View', [-16.7000  -90.0000]);
     
 end
+
 
