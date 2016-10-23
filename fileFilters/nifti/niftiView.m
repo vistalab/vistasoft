687856b8d6599e872c9b7a22bef9739695d51a43
Brian Wandell <wandell@stanford.edu>
2016-10-23 14:50:20 -0700

    Comments added

--- fileFilters/nifti/niftiView.m
+++ fileFilters/nifti/niftiView.m
@@ -1,17 +1,34 @@
 function [montage, hdl] = niftiView(ni,varargin)
 % Read a NIfTI file or a nifti struct and display one of its volumes
 %
-%    hdl = niftiView(ni,'volume',number,'figure',handle,'gam',gamma);
+%    hdl = niftiView(ni,'volume',number,'hdl',handle,'gam',gamma);
+%
+% Input:
+%    ni:  Either a nifti file or a nifti struct
+%
+% Parameter/values
+%    volume:  Number of the volume to display
+%    hdl:     Graphics handle to the display window
+%               (default mrvNewGraphWin);
+%    gam:     Gamma value for display (default 0.5)
+%
+% Returns:
+%    montage:  The image in the window
+%    hdl:      The returned figure handle 
 %
 % RL/BW Vistasoft Team, 2016
+
+%% Parse parameters
 
 p = inputParser;
 p.KeepUnmatched = true;
 
+% A file or a nifti struct
 vFunc = @(x) ((isstruct(x) && isfield(x,'nifti_type')) || (ischar(x) && exist(x,'file')));
 p.addRequired('ni',vFunc);
 if ischar(ni), ni = niftiRead(ni);  end
 
+% Parameter/values
 p.addParameter('volume',1,@isnumeric);
 p.addParameter('hdl',[],@isgraphics);
 p.addParameter('gam',0.5,@isnumeric);
@@ -21,8 +38,10 @@ volume = p.Results.volume;
 hdl    = p.Results.hdl;     %#ok<NASGU>
 gam    = p.Results.gam;
 
-% Show it as montage.  Be alert that we are treating 3D data as if it could
-% have a fourth volume dimension, but make sure that volume is always 1.
+%% Show data as montage.  
+
+% Be alert that we are treating 3D data as if it could have a fourth volume
+% dimension, but make sure that volume is always 1.
 if length(ni.dim) == 3 && volume ~= 1
     error('Volume parameter out of range')
 end
