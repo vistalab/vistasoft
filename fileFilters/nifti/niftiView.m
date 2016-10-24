4d4b86235758e498eea66ae58e47179cae056fc9
Brian Wandell <wandell@stanford.edu>
2016-10-23 20:42:20 -0700

    Added ability for nitfiView to show one slice. Shortened file name (which can be quite long) to first 10 letters of the name.

--- fileFilters/nifti/niftiView.m
+++ fileFilters/nifti/niftiView.m
@@ -7,7 +7,8 @@ function [montage, hdl] = niftiView(ni,varargin)
 %    ni:  Either a nifti file or a nifti struct
 %
 % Parameter/values
-%    volume:  Number of the volume to display
+%    volume:  Which volume to display.  
+%    slice:   Restrict to a slice
 %    hdl:     Graphics handle to the display window
 %               (default mrvNewGraphWin);
 %    gam:     Gamma value for display (default 0.5)
@@ -30,6 +31,7 @@ if ischar(ni), ni = niftiRead(ni);  end
 
 % Parameter/values
 p.addParameter('volume',1,@isnumeric);
+p.addParameter('slice',[],@isnumeric);
 p.addParameter('hdl',[],@isgraphics);
 p.addParameter('gam',0.5,@isnumeric);
 
@@ -37,20 +39,43 @@ p.parse(ni,varargin{:});
 volume = p.Results.volume;
 hdl    = p.Results.hdl;     %#ok<NASGU>
 gam    = p.Results.gam;
+slice  = p.Results.slice;   % Which slice?
 
-%% Show data as montage.  
+%% Either bring up the whole volume or just a slice from one volume
 
-% Be alert that we are treating 3D data as if it could have a fourth volume
-% dimension, but make sure that volume is always 1.
-if length(ni.dim) == 3 && volume ~= 1
-    error('Volume parameter out of range')
+% This is the relevant volume, gamma corrected
+v = squeeze(double(ni.data(:,:,:,volume))).^gam;
+
+if isempty(slice)
+    % Show multiple slices in a volume as montage.
+    
+    % Be alert that we are treating 3D data as if it could have a fourth volume
+    % dimension, but make sure that volume is always 1.
+    if length(ni.dim) == 3 && volume ~= 1
+        error('Volume parameter out of range')
+    end
+    [montage, hdl] = niftiMontage(v,varargin{:});
+    
+    % Label the image
+    [~,fname,~] = fileparts(ni.fname);
+    fname = strrep(fname,'_','-');
+    sz = size(ni.data);
+    title(sprintf('%s (%d,%d,%d) (Vol=%d)',fname(1:10),sz(1),sz(2),sz(3),volume));
+else
+    % Show one slice from one volume of the nifti file
+    
+    % We should allow a way to specify different slices, not just the 3rd
+    % dimension
+    mrvNewGraphWin;
+    montage = v(:,:,slice);
+    imagesc(montage);
+    colormap(gray); axis image;
+    
+    % Label the image.  Sometimes the label is too long.
+    [~,fname,~] = fileparts(ni.fname);
+    fname = strrep(fname,'_','-');
+    sz = size(ni.data);
+    title(sprintf('%s (%d,%d,%d) (V=%d, S=%d)',fname(1:10),sz(1),sz(2),sz(3),volume,slice));
 end
-[montage, hdl] = niftiMontage((squeeze(double(ni.data(:,:,:,volume))).^gam),varargin{:});
-
-% Label the image
-[~,fname,~] = fileparts(ni.fname);
-fname = strrep(fname,'_','-');
-sz = size(ni.data);
-title(sprintf('File %s (%d,%d,%d) (Vol=%d)',fname,sz(1),sz(2),sz(3),volume));
 
 end
