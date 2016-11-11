1666471ccf3c8786bbbf7888ab76bab147253fe4
Rosemary Le <rkimle@black>
2016-11-10 21:01:48 -0800

    Add isgraphics() to check the output of mrvNewGraphWin in the dtiCheckMotion function

--- mrDiffusion/preprocess/dtiCheckMotion.m
+++ mrDiffusion/preprocess/dtiCheckMotion.m
@@ -38,7 +38,9 @@ t = vertcat(ec.xform(:).ecParams);
 % We make a plot of the motion correction during eddy current correction
 % but we do not show the figure. We only save it to disk.
 fh = mrvNewGraphWin([],[],visibility);
-fh = fh.Number; 
+if isgraphics(fh)
+    fh = fh.Number; 
+end
 
 subplot(2,1,1); 
 plot(t(:,1:3)); 
