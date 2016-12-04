8c9d9514f9f908c93ab8107d29d516c13d8023e1
Jonathan Winawer <jonathan.winawer@nyu.edu>
2016-12-04 13:55:09 -0500

    fix export of volumetric data as niftis

--- mrBOLD/XformView/functionals2nifti.m
+++ mrBOLD/XformView/functionals2nifti.m
@@ -25,26 +25,16 @@ if notDefined('scan'), scan = viewGet(vw, 'current scan'); end
 % Extract the data being displayed and the corresponding locations
 data = getCurData(vw, viewGet(vw, 'displayMode'), scan);
 
-% Convert mrVista format to our preferred axial format for NIFTI
-mmPerVox = viewGet(vw, 'mmPerVox');
-data = mrLoadRet2nifti(data, mmPerVox); 
-
-% Replace NaNs with 0så
+% Replace NaNs with 0s
 data(isnan(data)) = 0;
 
-%Load t1 to get xforms correct
-t1 = niftiRead(vANATOMYPATH);
-qto = niftiGet(t1, 'qto_xyz');
-
-% Create nifti to export with transform matched to t1
-ni = niftiCreate('data', data, 'qto_xyz', qto);
-
 % name and path to save file
-if notDefined('outpth'), ni.fname = fullfile(fileparts(vANATOMYPATH), ['functionalOverlay-' datestr(now,1) '.nii.gz']);
-else  ni.fname = outpth; end
-    
-writeFileNifti(ni);
+if notDefined('outpth'), fname = fullfile(fileparts(vANATOMYPATH), ['functionalOverlay-' datestr(now,1) '.nii.gz']);
+else  fname = outpth; end
 
-fprintf('Functional data saved as %s.\n', ni.fname);
+% Create and save nifti file with data
+fname = niftiSaveVistaVolume(vw, data, fname);
+    
+fprintf('Functional data saved as %s.\n', fname);
 
 return;
