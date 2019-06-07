96e357766fc36e51e29280c6b27ae32e077cbd67
Brian Wandell <wandell@stanford.edu>
2019-06-07 14:37:52 -0700

    New parameterization of ellipsePlot

--- mrBOLD/Stats/ellipse/ellipsePlot.m
+++ mrBOLD/Stats/ellipse/ellipsePlot.m
@@ -1,25 +1,36 @@
-function [x,y] = ellipsePlot(a,b,theta,radSpacing)
+function [x,y] = ellipsePlot(theta, center, axesSize, radSpacing)
 % Make samples on an ellipse
 %
-%  [x,y] = ellipsePlot(a,b,theta,radSpacing,closeEllipse)
+% Syntax:
+%  [x,y] = ellipsePlot(theta, center, axesSize, radSpacing)
+%
+% Brief description:
+%   Plots an ellipse in the plane.  Not sure why theta is in radians
+%   and everything else in degrees.  Maybe I should change that
 %
 % Inputs:
-%  (a,b): Major and minor axes, x-scale, y-scale
-%  theta:  Angle in radians, clockwise
 %
-% Example
+%  theta:      Angle in radians, clockwise
+%  center:     Center in degrees
+%  axesSize:   Major and minor axes in degrees
+%  radSpacing: Spacing of point samples in radians
+%
+% Example:
 %{
-    a = 1; b = 2; theta = pi/3; radSpacing = 0.1;
-    [x,y] = ellipsePlot(a,b,theta,radSpacing);
+    axesSize = [1,1.5]; center = [1,1]; theta = pi/3; radSpacing = 0.1;
+    [x,y] = ellipsePlot(theta,center, axesSize, radSpacing);
 %}
 % (c) Stanford VISTA  Team
 
-if notDefined('a'), error('a Parameter required'); end
-if notDefined('b'), error('b Parameter required'); end
 if notDefined('theta'), theta = 0; end
+if notDefined('center'), center = [0,0]; end
+if notDefined('axesSize'), axesSize = [1,1]; end
+if notDefined('radSpacing'), radSpacing = 0.1; end
 
 % Points for a closed ellipse
-[x,y] = ellipsePoints(a,b,theta,radSpacing,true);
+[x,y] = ellipsePoints(axesSize(1),axesSize(2),theta,radSpacing,true);
+x = center(2) + x;
+y = center(1) + y;
 
 % Bring up one of the local window methods
 if     exist('mrvNewGraphWin','file'), mrvNewGraphWin;
@@ -27,5 +38,7 @@ elseif exist('ieNewGraphWin','file'),  ieNewGraphWin;
 end
 
 plot(x,y,'-'); axis square; grid on
+mx = max(x(:)); my = max(y(:));
+set(gca,'xlim',[-1*mx, mx],'ylim',[-1*my,my]);
 
 end
