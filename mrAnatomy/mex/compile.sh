fcdea5b8e4eb9d4d600098b0131b8a957a3a5d65
Soichi Hayashi <hayashis@iu.edu>
2020-04-21 01:17:36 +0000

    updated myCinterp3 so that it can handle half-precision input

--- mrAnatomy/mex/compile.sh
+++ mrAnatomy/mex/compile.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-set -e
-set -x
+
 module load matlab/2019a
 module unload gcc
 module load gcc/6.1.0
-#mex -O Csource/nearpoints32.cxx
-mex -O Csource/myCinterp3.c
+
+set -e
+set -x
+#mex -O Csource/nearpoints.cxx
+mex -O Csource/myCinterp3.cxx
