c5f0120b339fe54a0c81b565f8bba6a0b70a32d0
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-08-30 11:01:23 -0700

    Update t_meshShow.m

--- tutorials/mesh/t_meshShow.m
+++ tutorials/mesh/t_meshShow.m
@@ -2,6 +2,8 @@
 %
 % This script loads a mesh and calls mrMesh to show it.
 % 
+% To download the test data, including the mrvistadata information,
+% look at mrtInstallSampleData
 %
 % See also:  t_meshFromClass
 %
@@ -12,8 +14,8 @@ tmp = load(fullfile(mrvDataRootPath,'anatomy','anatomyNIFTI','leftMesh.mat'));
 msh = tmp.msh; clear tmp
 msh = meshSet(msh,'actor',33);
 
-windowID = 1000;
-msh = meshSet(msh,'window id',1000);
+windowID = 999;
+msh = meshSet(msh,'window id',999);
 meshVisualize(msh);
 
 %% End
